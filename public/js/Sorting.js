e414b2f159c122e1a9a4be99960f342ea29c133c
Mathias Gredal <Mathiasgredal@icloud.com>
2017-04-14 18:19:09 +0200

    Bug Fixes
    Fixed a bug where when you entered a page there would be two of every
    video. Fixed another stupid mistake so now the arrow works both ways
    and you can now exit the modal with the escape key

--- public/js/Sorting.js
+++ public/js/Sorting.js
@@ -1,130 +1,106 @@
 var isAscending = false;
 
-function SortArray( temptempVideoInfo, sortType )
-{
-	var temptemptempVideoInfo = new Array();
+function SortArray( temptempVideoInfo, sortType ) {
 
-	switch ( sortType )
-	{
-	case "Name":
-		NameSort( temptempVideoInfo, function ( sortedArray )
-		{
-			temptemptempVideoInfo = sortedArray;
-		} );
-		break;
+	var temptemptempVideoInfo = new Array( );
 
-	case "Skip Seconds":
-		SkipSecondsSort( temptempVideoInfo, function ( sortedArray )
-		{
-			temptemptempVideoInfo = sortedArray;
-		} );
-		break;
+	switch ( sortType ) {
+		case "Name":
+			NameSort( temptempVideoInfo, function ( sortedArray ) {
+				temptemptempVideoInfo = sortedArray;
+			});
+			break;
 
-	case "Date":
-		DateSort( temptempVideoInfo, function ( sortedArray )
-		{
-			temptemptempVideoInfo = sortedArray;
-		} );
-		break;
+		case "Skip Seconds":
+			SkipSecondsSort( temptempVideoInfo, function ( sortedArray ) {
+				temptemptempVideoInfo = sortedArray;
+			});
+			break;
 
+		case "Date":
+			DateSort( temptempVideoInfo, function ( sortedArray ) {
+				temptemptempVideoInfo = sortedArray;
+			});
+			break;
 
-	default:
+		default:
 
 	}
 
 }
 
+function NameSort( videoInfoArray, callback ) {
 
-
-function NameSort( videoInfoArray, callback )
-{
 	var sortedArray = videoInfoArray;
 
-	sortedArray.sort( function ( a, b )
-	{
+	sortedArray.sort( function ( a, b ) {
 		var str1 = a;
 		var str2 = b;
 
 		var substringedName1 = str1.name.substring( 0, 13 );
 		var substringedName2 = str2.name.substring( 0, 13 );
 
-		if ( substringedName1 == "[Hearthstone]" )
-		{
+		if ( substringedName1 == "[Hearthstone]" ) {
 			str1.name = str1.name.substring( 14 );
 		}
-		if ( substringedName2 == "[Hearthstone]" )
-		{
+		if ( substringedName2 == "[Hearthstone]" ) {
 			str2.name = str2.name.substring( 14 );
 		}
 
-
-
-		if ( str1.name.toLowerCase() < str2.name.toLowerCase() )
-		{
+		if (str1.name.toLowerCase( ) < str2.name.toLowerCase( )) {
 			return -1;
 		}
-		if ( str1.name.toLowerCase() > str2.name.toLowerCase() )
-		{
+		if (str1.name.toLowerCase( ) > str2.name.toLowerCase( )) {
 			return 1;
 		}
 		return 0;
 
-	} );
+	});
 	callback( sortedArray );
 }
 
-function SkipSecondsSort( videoInfoArray, callback )
-{
+function SkipSecondsSort( videoInfoArray, callback ) {
 	var sortedArray = videoInfoArray;
 
-
-	sortedArray.sort( function ( a, b )
-	{
+	sortedArray.sort( function ( a, b ) {
 		var str1 = a;
 		var str2 = b;
 
 		var substringedName1 = str1.name.substring( 0, 13 );
 		var substringedName2 = str2.name.substring( 0, 13 );
 
-
-		if ( substringedName1 === "[Hearthstone]" )
-		{
+		if ( substringedName1 === "[Hearthstone]" ) {
 			str1.name = str1.name.substring( 13 );
 		}
-		if ( substringedName2 === "[Hearthstone]" )
-		{
+		if ( substringedName2 === "[Hearthstone]" ) {
 			str2.name = str2.name.substring( 13 );
 		}
 
 		return a.skipSeconds - b.skipSeconds;
-	} );
+	});
 	callback( sortedArray );
 }
 
-function DateSort( videoInfoArray, callback )
-{
+function DateSort( videoInfoArray, callback ) {
+
 	var sortedArray = videoInfoArray;
 
-	sortedArray.sort( function ( a, b )
-	{
+	sortedArray.sort( function ( a, b ) {
 		var str1 = a;
 		var str2 = b;
 
 		var substringedName1 = str1.name.substring( 0, 13 );
 		var substringedName2 = str2.name.substring( 0, 13 );
 
-
-		if ( substringedName1 === "[Hearthstone]" )
-		{
+		if ( substringedName1 === "[Hearthstone]" ) {
 			str1.name = str1.name.substring( 13 );
 		}
-		if ( substringedName2 === "[Hearthstone]" )
-		{
+		if ( substringedName2 === "[Hearthstone]" ) {
 			str2.name = str2.name.substring( 13 );
 		}
 
 		return new Date( b.date ) - new Date( a.date );
 
-	} );
+	});
 	callback( sortedArray );
 }
