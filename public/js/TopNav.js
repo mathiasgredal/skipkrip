16cdef608f7afbca3d8bcc8dec6623702f12585b
Mathias Gredal <Mathiasgredal@icloud.com>
2017-04-14 16:07:37 +0200

    Fixed mobile design mode

--- public/js/TopNav.js
+++ public/js/TopNav.js
@@ -1,45 +1,6 @@
-/*
-var button1 = "<a href = \"index.html\" > Skip The Kripp</a>"
-var button2 = "<a href = \"All Videos.html\" > All The Videos</a>"
-var button3 = "<a href = \"Missing Timestamps.html\" > Jobs For Skipperino</a>"
-var button4 = "<a href = \"#about\" >About</a>"
-var javascript = "<a href = \"javascript:void(0);\"class = \"icon\"onclick = \"myFunction()\">&#9776;</a>"
-
-//var topNavInnerHTML = button1 + button2 + button3 + button4 + javascript;
-var topNavInnerHTML = ""
-
-document.getElementById( "myTopnav" )
-	.innerHTML = topNavInnerHTML;
-
-function myFunction()
-{
-	var x = document.getElementById( "myTopnav" );
-	if ( x.className === "topnav" )
-	{
-		x.className += " responsive";
-	}
-	else
-	{
-		x.className = "topnav";
-	}
-}
-*/
-/*
-// this sets the class select to the page we are on
-$( function ( ) {
-	$( "a" ).each( function ( ) {
-		if ( $( this ).attr( "href" ) == window.location.pathname ) {
-			$( this ).addClass( "select" );
-		}
-	});
-});
-*/
-
 var list = $( "#NavElementList" );
 
 var childs = list[0].children;
-
-//if (!$( list ).hasClass( "responsive" )) {
 
 MoveToCorrectPage( );
 
@@ -57,19 +18,13 @@ list.mouseleave( function ( ) {
 
 function MoveToCorrectPage( ) {
 	$( ".border" ).css( "transform", "translateX(" + (GetDistance($( ".select" ).index( )) + ( $( ".select" ).outerWidth( true ) / 2 )) + "px)" + " scaleX(" + $( ".select" ).outerWidth( true ) + ")" );
-	// add support for webkit
 	$( ".border" ).css( "-webkit-transform", "translateX(" + (GetDistance($( ".select" ).index( )) + ( $( ".select" ).outerWidth( true ) / 2 )) + "px)" + " scaleX(" + $( ".select" ).outerWidth( true ) + ")" );
 
 }
 
 function MoveToSelectedPage( currentElement ) {
 	$( ".border" ).css( "transform", "translateX(" + (GetDistance($( currentElement ).index( )) + ( $( currentElement ).outerWidth( true ) / 2 )) + "px)" + " scaleX(" + $( currentElement ).outerWidth( true ) + ")" );
-	// add support for webkit
 	$( ".border" ).css( "-webkit-transform", "translateX(" + (GetDistance($( currentElement ).index( )) + ( $( currentElement ).outerWidth( true ) / 2 )) + "px)" + " scaleX(" + $( currentElement ).outerWidth( true ) + ")" );
-
-	//} else {
-	// it is responsive design mode
-	//$( ".border" ).css( "display", "none" );
 }
 
 function GetDistance( index ) {
